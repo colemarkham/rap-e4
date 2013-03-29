87be521af82322bcbb29ed9f88e260f3b3c8dad0
Cole Markham <cole@ccmcomputing.net>
2013-03-29 13:18:15 -0500

    Sample E4 Application running on RAP

---
+++ e4rap.sample/Readme.md
@@ -0,0 +1,4 @@
+e4rap.sample
+======
+
+This is a very basic E4 sample application which was created using the E4 Tooling. The file open handler was modified to remove the reference to FileDialog. It should be possible to add the FileDialog from the RAP Incubator. This application should run as-is on E4 SWT as well, just change the target-platform accordingly.  
---
+++ e4rap.sample/src/e4rap/sample/handlers/AboutHandler.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package e4rap.sample.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class AboutHandler {
+	
+	public AboutHandler() {
+		System.out.println("AboutHandler constructor");
+	}
+	
+	@Execute
+	public void execute(MWindow window) {
+		MessageDialog.openInformation((Shell)window.getWidget(), "About", "Eclipse 4 Application example.");
+	}
+}
